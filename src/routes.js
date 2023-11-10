2dc7649f45063ba063da60938a5372f5447042b2
Matheussmaced <mateusbyakuya3@gmail.com>
2023-11-09 21:03:09 -0300

    feat: creating regex

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 import { Database } from "./database.js";
 import { randomUUID } from 'node:crypto';
+import { buildRoutePath } from "./utils/build-route-path.js";
 
 const database = new Database
 
 export const routes = [
   {
     method: 'GET',
-    path: '/tasks',
+    path: buildRoutePath('/tasks'),
     handler: (req, res) => {
       const tasks = database.select('tasks')
 
@@ -15,7 +16,7 @@ export const routes = [
   },
   {
     method: 'POST',
-    path: '/tasks',
+    path: buildRoutePath('/tasks'),
     handler: (req, res) => {
       const { name, status } = req.body
 
@@ -32,7 +33,7 @@ export const routes = [
   },
   {
     method: 'DELETE',
-    path: '/tasks/:id',
+    path: buildRoutePath('/tasks/:id'),
     handler: (req, res) => {
       return res.end()
     }
---
+++ src/utils/build-route-path.js
@@ -0,0 +1,4 @@
+export function buildRoutePath(path) {
+  const routeParametersRegex = /:([a-zA-Z]+)/g
+
+}
